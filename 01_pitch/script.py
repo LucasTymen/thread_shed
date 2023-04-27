4607aad004d1413bfecd087e496f1ad69c6b7952
Lucas Tymen <lucas.tymen@gmail.com>
2023-04-27 23:14:25 +0200

    Thread Shed_20

--- 01_pitch/script.py
+++ 01_pitch/script.py
@@ -140,6 +140,18 @@ for transaction in transactions_clean:
 total_sales = 0
 for sale in sales:
   total_sales += float(sale.strip("$"))
-print(total_sales)
 
-print(thread_sold)
+
+thread_sold_split = []
+for sale in thread_sold:
+  for color in sale.split("&"):
+    thread_sold_split.append(color)
+
+def color_count(color):
+  color_total = 0
+  for color in thread_sold_split:
+    if color == thread_color:
+      color_total += 1
+  return color_total
+
+print(color_count('white'))
